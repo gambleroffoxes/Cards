98d60ec59ebf94ef0aa9bb947cf71deda331fe78
Peter Colletti <gambleroffoxes@gmail.com>
2011-06-21 12:43:27 +0200

    added addcard to pile and first commit of main.js

--- Pile.js
+++ Pile.js
@@ -2,7 +2,16 @@ function Pile(size){
 	this.size = size;
 }
 
+//instantiate as an array
+
 Pile.prototype = {
+
+	addCard: function(c){
+		//add card c to the counter's position + 1 and add 1 to the counter
+		this[this.size + 1] = c
+		this.size++
+	}
+
 	toString: function(){
 		return "This pile contains " + this.size + " card" + (this.size == 1 ? "." : "s.");
 	}
