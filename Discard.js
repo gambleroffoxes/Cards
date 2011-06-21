4f3b1fac43ebdd23016f72f0dfa03592e59397b3
Peter Colletti <gambleroffoxes@gmail.com>
2011-06-21 13:49:55 +0200

    moved list and search to pile, discard is deprecated now

--- Discard.js
+++ Discard.js
@@ -5,38 +5,6 @@ function Discard(size){
 
 Discard.prototype = new Pile(this.size)
 
-	list: function(){
-		//list all cards in the discard pile in order from top to bottom
-		var listCounter = 0;
-		var cardList = "This discard pile contains 0 cards.";
-		while listCounter < this.size {
-			if listCounter = 0
-				cardList = this.discard[0];
-			else cardList += ", " + this.discard[listCounter];
-			listCounter++;
-		}
-		return cardList;
-	},
-
-	search: function(target){
-		//search the discard pile for a particular card
-		var searcher = 0;
-		var currentCard = this.discard[searcher];
-		var msg = "This discard pile contains 0 cards.";
-		if this.size == 0 {
-			return msg;
-			break;
-		}
-		while searcher < this.size {
-			if target == currentCard {
-				return "The " + target.rank + " of " + target.suit + " is the number " + (searcher + 1) + " card in the discard pile.";
-				break;
-			}
-			searcher++;
-		}
-		return "The card you are searching for cannot be found."
-	},
-
 	toString: function(){
 		return "This discard pile contains " + this.size + " card" + (this.size == 1 ? "." : "s.");
 	}
--- Pile.js
+++ Pile.js
@@ -6,10 +6,40 @@ function Pile(size){
 Pile.prototype = {
 
 	addCard: function(c){
-		//add card c to the counter's position + 1 and add 1 to the counter
+		//add card c to the pile
 		this.pile[this.size + 1] = c;
 		this.size++;
 	},
+
+	list: function(){
+		//list all cards in the pile in order from top to bottom
+		var listCounter = 0;
+		var cardList = "This pile contains 0 cards.";
+		while (listCounter < this.size) {
+			if (listCounter = 0)
+				cardList = this.pile[0];
+			else cardList += ", " + this.pile[listCounter];
+			listCounter++;
+		}
+		return cardList;
+	},
+
+	search: function(target){
+		//search the pile for a particular card
+		var searcher = 0;
+		var currentCard = this.pile[searcher];
+		var msg = "This pile contains 0 cards.";
+		if (this.size == 0) return msg;
+		while (searcher < this.size) {
+			if (target == currentCard) {
+				return "The " + target.rank + " of " + target.suit + " is the number " + (searcher + 1) + " card in the pile.";
+				break;
+			}
+			searcher++;
+		}
+		return "The card you are searching for cannot be found.";
+	},
+
 
 	toString: function(){
 		return "This pile contains " + this.size + " card" + (this.size == 1 ? "." : "s.");
