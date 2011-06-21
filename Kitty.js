0785cb6ff06ce6e5e81f3b0d22010b16cc21f71f
Peter Colletti <gambleroffoxes@gmail.com>
2011-06-21 13:17:54 +0200

    first commit of kitty with topcard

---
+++ Kitty.js
@@ -0,0 +1,25 @@
+function Kitty(size){
+	this.size = size;
+	this.kitty = new Array(size);
+}
+
+Kitty.prototype = new Pile(this.size)
+
+	topCard: function(n){
+		//displays the top n cards of the kitty
+		if n == 0 return "You have not asked to display any cards from the kitty.";
+		var counter = 0;
+		var list = "The top " + n + " cards of the kitty are: ";
+		while counter < n {
+			if counter == 0 {
+				list += this.kitty[counter].rank + " of " + this.kitty[counter].suit + "s";
+			}
+			else list += ", " + this.kitty[counter].rank + " of " + this.kitty[counter].suit + "s";
+		}
+		return list;
+	}
+
+	toString: function(){
+		return "This deck contains " + this.size + " card" + (this.size == 1 ? "." : "s.");
+	}
+}
