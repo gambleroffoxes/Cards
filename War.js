ced8317aecb80383c4c1a87613675f5264e08e2d
Peter Colletti <gambleroffoxes@gmail.com>
2011-06-23 00:50:18 +0200

    trying to make a working version of war in progress

--- War.js
+++ War.js
@@ -6,6 +6,20 @@ War.prototype.inherits(Table);
 
 War.prototype.extend({
 
+	war: function(hand1, card1, hand2, card2){
+		var isWar = this.take(hand1, card1, hand2, card2);
+		if (isWar == -1) {
+			this.battle();
+		}
+	},
+
+	battle: function(hand1, card1, hand2, card2){
+		var warAgain = this.take(hand1, card1, hand2, card2);
+		if (warAgain == -1) {
+			this.battle();
+		}
+	},
+
 	toString: function(){
 		return "This is a game of war with " + this.players + " players.";
 	}
