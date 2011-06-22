42cb37fe976bc3ed9d6eac776bc2954f374e9076
Peter Colletti <gambleroffoxes@gmail.com>
2011-06-22 22:30:27 +0200

    tested pile

--- Pile.js
+++ Pile.js
@@ -1,24 +1,29 @@
 function Pile(size){
 	this.size = size;
-	this.pile = new Array(size)
+	this.pile = new Array(size);
 }
 
 Pile.prototype = {
 
 	addCard: function(c){
 		//add card c to the pile
-		this.pile[this.size + 1] = c;
+		this.pile[this.size] = c;
 		this.size++;
 	},
 
 	list: function(){
+		//FIXME		
+		//look up string commands to make this easier: put ", " in front of everything then remove the first two characters
 		//list all cards in the pile in order from top to bottom
 		var listCounter = 0;
 		var cardList = "This pile contains 0 cards.";
 		while (listCounter < this.size) {
-			if (listCounter = 0)
-				cardList = this.pile[0];
-			else cardList += ", " + this.pile[listCounter];
+			if (listCounter == 0) {
+				cardList = this.pile[0].rank + " of " + this.pile[0].suit + "s";
+			}
+			else {
+				cardList += ", " + this.pile[listCounter].rank + " of " + this.pile[listCounter].suit + "s";
+			}
 			listCounter++;
 		}
 		return cardList;
@@ -27,12 +32,13 @@ Pile.prototype = {
 	search: function(target){
 		//search the pile for a particular card
 		var searcher = 0;
-		var currentCard = this.pile[searcher];
+		var currentCard = null;
 		var msg = "This pile contains 0 cards.";
 		if (this.size == 0) return msg;
 		while (searcher < this.size) {
+			currentCard = this.pile[searcher];
 			if (target == currentCard) {
-				return "The " + target.rank + " of " + target.suit + " is the number " + (searcher + 1) + " card in the pile.";
+				return "The " + target.rank + " of " + target.suit + "s is the number " + (searcher + 1) + " card in the pile.";
 				break;
 			}
 			searcher++;
